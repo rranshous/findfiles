c72b98359140e86e48575f8f3eb5df730fa96f8d
Robby Ranshous <rranshous@gmail.com>
2011-06-24 17:13:15 -0400

    added dir searching

--- __init__.py
+++ __init__.py
@@ -1,5 +1,23 @@
 import os
 
+def find_dirs_iter(path,exclude=None):
+    """ yield back dir paths meeting extension/exclude criteria """
+
+    # absolute paths
+    found = []
+
+
+    for dir_path, dir_names, file_names in os.walk(path):
+        # remove paths which include the exclude string
+        # this will keep them from being traversed
+        bad_dirs = [x for x in dir_names if exclude in dir_names]
+        map(dir_names.remove,bad_dirs)
+
+        # see if any of the current files meet our
+        # extension and exclude criteria
+        for name in dir_names:
+            if (not exclude or exclude not in x):
+                yield os.path.abspath(os.path.join(dir_path,name))
 
 def find_files_iter(path,extension=None,exclude=None,file_name=None):
     """ yield back file paths meeting extension/exclude criteria """
@@ -29,5 +47,9 @@ def find_files_iter(path,extension=None,exclude=None,file_name=None):
 
 def find_files(path,extension=None,exclude=None,file_name=None):
     r = [f for f in find_files_iter(path,extension,exclude,file_name)]
+    return r
+
+def find_dirs(path,exclude=None):
+    r = [f for f in find_dirs_iter(path,exclude)]
     return r
 
