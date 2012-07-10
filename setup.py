4ed84d1290640a630f572998f9cf1c29f49cc21e
Robby Ranshous <rranshous@gmail.com>
2012-07-10 11:12:47 -0400

    refactor file layout, adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import find_files
+
+requirements = [
+]
+
+setup(
+    name="findfiles",
+    version=find_files.__version__,
+    author="Robby Ranshous",
+    author_email="rranshous@gmail.com",
+    description="simple dead resource finder",
+    keywords="find",
+    url="https://github.com/rranshous/findfiles",
+    py_modules=["find_files"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    install_requires=requirements,
+)
