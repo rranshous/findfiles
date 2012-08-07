c8e7161587818105f00d0581a6a6ae8027ead356
Robby Ranshous <rranshous@gmail.com>
2012-08-07 14:02:38 -0400

    updated to include globbing of source path

--- findfiles.py
+++ findfiles.py
@@ -1,54 +1,91 @@
 
 import os
+import glob
 
 version_info = (0, 0, 5)
 __version__ = '.'.join(map(str, version_info))
 version = __version__
 
 def find_dirs_iter(path,exclude=None):
-    """ yield back dir paths meeting extension/exclude criteria """
+    """
+    yield back dir (abs) paths meeting extension/exclude criteria
 
-    # absolute paths
-    found = []
+    path can be a glob-able path (contains a *) in which case all
+    paths resulting from the glob will be considered source paths
 
+    exclude should be a string which should not be part of
+    a matching file's name
+    """
 
-    for dir_path, dir_names, file_names in os.walk(path):
-        # remove paths which include the exclude string
-        # this will keep them from being traversed
-        bad_dirs = [x for x in dir_names if exclude in dir_names]
-        map(dir_names.remove,bad_dirs)
+    # go through all the paths resulting from interpreting
+    # the path as a glob
+    for source_path in glob.iglob(path):
 
-        # see if any of the current files meet our
-        # extension and exclude criteria
-        for name in dir_names:
-            if (not exclude or exclude not in x):
+        # go through all the files / dirs off the source path
+        for dir_path, dir_names, file_names in os.walk(source_path):
+
+            # remove paths which include the exclude string
+            # this will keep them from being traversed
+            bad_dirs = [x for x in dir_names if exclude in dir_names]
+            map(dir_names.remove,bad_dirs)
+
+            # see if any of the current files meet our
+            # extension and exclude criteria
+            for name in dir_names:
+
+                # does this dir name include our excluded string?
+                if exclude and exclude in name:
+                    continue
+
+                # yield back the absolute path
                 yield os.path.abspath(os.path.join(dir_path,name))
 
 def find_files_iter(path,extension=None,exclude=None,file_name=None):
-    """ yield back file paths meeting extension/exclude criteria """
+    """
+    yield back (abs) file paths meeting extension/exclude criteria
 
-    # absolute paths
-    found = []
+    path can be a glob-able path (contains a *) in which case all
+    paths resulting from the glob will be considered source paths
 
+    extension should start with a '.' if appropriate
 
-    for dir_path, dir_names, file_names in os.walk(path):
-        # remove paths which include the exclude string
-        # this will keep them from being traversed
-        bad_dirs = [x for x in dir_names if exclude in dir_names]
-        map(dir_names.remove,bad_dirs)
+    exclude should be a string which should not be part of
+    a matching file's name
+    """
 
-        # see if any of the current files meet our
-        # extension and exclude criteria
-        for name in file_names:
-            # if we specified a file name than we need to
-            # check for that match first
-            if file_name is not None:
-                if file_name != name:
+    # go through all the paths resulting from interpreting
+    # the path as a glob
+    for source_path in glob.iglob(path):
+
+        # go through all the files / dirs off the source path
+        for dir_path, dir_names, file_names in os.walk(source_path):
+
+            # remove paths which include the exclude string
+            # this will keep them from being traversed
+            bad_dirs = [x for x in dir_names if exclude in dir_names]
+            map(dir_names.remove,bad_dirs)
+
+            # see if any of the current files meet our
+            # extension and exclude criteria
+            for name in file_names:
+
+                # if we specified a file name than we need to
+                # check for that match first
+                if file_name is not None and file_name != name:
                     continue
 
-            if (not extension or name.endswith(extension)) \
-               and (not exclude or exclude not in x):
-                yield os.path.abspath(os.path.join(dir_path,name))
+                # if there is a required extension specified
+                # make sure the file meets the criteria
+                if extension and not name.endswith(extension):
+                    continue
+
+                # if there is an excluded string, make sure the
+                # file name does not include it
+                if exclude and exclude in name:
+                    continue
+
+                # yield up our resulting file as an absolute path
+                yield os.path.abspath(os.path.join(dir_path, name))
 
 def find_files(path,extension=None,exclude=None,file_name=None):
     r = [f for f in find_files_iter(path,extension,exclude,file_name)]
